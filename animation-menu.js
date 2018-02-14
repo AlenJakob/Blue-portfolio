0702cf9389ad7497a99ee07e31ce04975659dc3d
Alen <33994452+AlenJakob@users.noreply.github.com>
2018-02-14 15:42:01 +0100

    Create animation-menu.js

---
+++ animation-menu.js
@@ -0,0 +1,25 @@
+.photo-small-slider{
+ background: url(https://cdn.pixabay.com/photo/2017/10/09/19/29/food-photography-2834549__340.jpg)!important;
+    background-position:center!important; 
+     background-size: cover!important;
+    
+}
+.photo-small-slider:hover{
+    box-shadow: inset 0 0 220px rgba(0,110,110,.4)!important;
+}
+.photo-small-slider div{ display: flex;
+width:100%;
+    height: 100%;
+}
+.photo-small-slider div i{
+    width:15px;
+    height: 15px;
+    border-radius: 50%;
+    background: white;
+    margin:4px 3px;
+    box-shadow: 0 0 4px black;
+}
+.photo-small-slider div i:hover{
+    background:DeepPink ;
+    box-shadow:inset 0 0 2px white;
+}
