89a1a34fbe57bfdc39eff207eaf921ee1d7aebf1
Alen <33994452+AlenJakob@users.noreply.github.com>
2018-02-14 15:42:19 +0100

    Update animation-menu.js

--- animation-menu.js
+++ animation-menu.js
@@ -1,25 +1,21 @@
-.photo-small-slider{
- background: url(https://cdn.pixabay.com/photo/2017/10/09/19/29/food-photography-2834549__340.jpg)!important;
-    background-position:center!important; 
-     background-size: cover!important;
+
+document.addEventListener('DOMContentLoaded', function() {
+  var clas = document.querySelectorAll('li');
+  for (let i=0; i< clas.length; i++) {
     
-}
-.photo-small-slider:hover{
-    box-shadow: inset 0 0 220px rgba(0,110,110,.4)!important;
-}
-.photo-small-slider div{ display: flex;
-width:100%;
-    height: 100%;
-}
-.photo-small-slider div i{
-    width:15px;
-    height: 15px;
-    border-radius: 50%;
-    background: white;
-    margin:4px 3px;
-    box-shadow: 0 0 4px black;
-}
-.photo-small-slider div i:hover{
-    background:DeepPink ;
-    box-shadow:inset 0 0 2px white;
-}
+    clas[i].addEventListener('mouseover', function() {
+
+   this.classList.add("animation");
+      
+  });
+      
+     clas[i].addEventListener('mouseout', function() {
+      this.classList.remove('animation');
+   
+      
+  });
+    
+     
+}  
+});
+
